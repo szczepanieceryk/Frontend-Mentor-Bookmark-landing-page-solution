0b9d6e0ceff38965a029af8e2fd453076bd7d548
Eryk Szczepaniec <szczepanieceryk@gmial.com>
2021-12-22 15:20:23 +0100

    add README.md, faq on click function

---
+++ src/README.md
@@ -0,0 +1,47 @@
+# Frontend Mentor - Bookmark landing page solution
+
+This is a solution to the [Bookmark landing page challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/bookmark-landing-page-5d0b588a9edda32581d29158). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Continued development](#continued-development)
+  - [Useful resources](#useful-resources)
+- [Author](#author)
+- [Acknowledgments](#acknowledgments)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the site depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Receive an error message when the newsletter form is submitted if:
+  - The input field is empty
+  - The email address is not formatted correctly
+
+### Links
+
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- Mobile-first workflow
+- [Angular](https://angular.io/) - JS framework
+
+## Author
+
+- Frontend Mentor - [@szczepanieceryk](https://www.frontendmentor.io/profile/szczepanieceryk)
--- src/app/faq/faq.component.ts
+++ src/app/faq/faq.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+// custom type for questionsAndAnswers Array
 type Faq = {
   question:string;
   answer:string;
+  idx:string;
 }
 
 @Component({
@@ -17,30 +19,44 @@ export class FaqComponent implements OnInit {
   faqTxt = "Here are some of our FAQs. If you have any other questions you’d like answered please feel free to email us.";
 
   // active flag for showAnswer function
-  isActive = false;
+  isActive :boolean = false;
+  // variable to findindex 
+  id:string = '';
 
   // content for FAQ card
   questionsAndAnswers:Array<Faq> = [
     {
+      idx:'f1',
       question:'What is Bookmark?',
       answer:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce tincidunt justo eget ultricies fringilla. Phasellus blandit ipsum quis quam ornare mattis.'
     },
     {
+      idx:'f2',
       question:'How can I request a new browser?',
       answer:'Vivamus luctus eros aliquet convallis ultricies. Mauris augue massa, ultricies non ligula. Suspendisse imperdiet. Vivamus luctus eros aliquet convallis ultricies. Mauris augue massa, ultricies non ligula. Suspendisse imperdie tVivamus luctus eros aliquet convallis ultricies. Mauris augue massa, ultricies non ligula. Suspendisse imperdiet.'
     },
     {
+      idx:'f3',
       question:'Is there a mobile app?',
       answer:'Sed consectetur quam id neque fermentum accumsan. Praesent luctus vestibulum dolor, ut condimentum urna vulputate eget. Cras in ligula quis est pharetra mattis sit amet pharetra purus. Sed sollicitudin ex et ultricies bibendum.'
     },
     {
+      idx:'f4',
       question:'What about other Chromium browsers?',
       answer:'Integer condimentum ipsum id imperdiet finibus. Vivamus in placerat mi, at euismod dui. Aliquam vitae neque eget nisl gravida pellentesque non ut velit.',
     },
   ]
 
-  showAnswer(){
-    this.isActive = !this.isActive;
+
+  // function for showing the answer for clicked question   
+  showAnswer(idx:string){
+    if(this.id == idx){
+      this.id = '';
+    }
+    else{
+      this.id = idx
+      this.isActive = !this.isActive
+    }
   }
 
   constructor() { }
